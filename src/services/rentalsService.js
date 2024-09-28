63099a36565cc3440300a5d98bff04b4f5f52f90
Matheus Henrique <matthpmoreira@gmail.com>
2024-09-28 05:02:15 -0300

    Refactor for clarity

--- src/services/rentalsService.js
+++ src/services/rentalsService.js
@@ -43,58 +43,59 @@ async function createRental(rental) {
 
     await customersService.getCustomerById(rental.customerId);
 
-    const rentDate = getSqlDate();
+    const rentDate = getCurrentDateSql();
     const originalPrice = rental.daysRented * game.pricePerDay;
 
     return rentalsRepository.createRental({ ...rental, rentDate, originalPrice });
 }
 
 async function returnRental(id) {
-    const result = await rentalsRepository.getRentalById(id);
-    const rental = result.rows[0];
-
-    if (result.rowCount === 0) {
-        throw new RentalNotFoundError(id);
-    }
+    const rental = await getRentalById(id);
 
     if (rental.returnDate != null) {
         throw new RentalReturnedError(id);
     }
 
-    const returnDate = getSqlDate();
-    const game = await gamesService.getGameById(rental.gameId);
-    const dayDiff = calcDayDiff(rental.rentDate, returnDate);
-    const delayFee = dayDiff > rental.daysRented ? game.pricePerDay * (dayDiff - rental.daysRented) : 0;
+    const returnDate = getCurrentDateSql();
+    const delayFee = await calcDelayFee(rental);
 
     return rentalsRepository.returnRental(id, returnDate, delayFee);
 }
 
-function getSqlDate() {
+function getCurrentDateSql() {
     const date = new Date;
     const dateStr = date.toISOString();
     return dateStr.slice(0, 10);
 }
 
-function calcDayDiff(rentTimestamp, returnTimestamp) {
-    const rentDate = new Date(rentTimestamp);
-    const returnDate = new Date(returnTimestamp);
+async function calcDelayFee(rental) {
+    const rentDate = new Date(rental.rentDate);
+    const returnDate = new Date();
     const microsecDiff = returnDate - rentDate;
-    return Math.floor(microsecDiff / (1000 * 60 * 60 * 24));
+    const dayDiff = Math.floor(microsecDiff / (1000 * 60 * 60 * 24));
+    const overdueDays = dayDiff - rental.daysRented;
+    const game = await gamesService.getGameById(rental.gameId);
+    return overdueDays > 0 ? game.pricePerDay * overdueDays : 0;
 }
 
 async function deleteRental(id) {
-    const result = await rentalsRepository.getRentalById(id);
-    const rental = result.rows[0];
-
-    if (result.rowCount === 0) {
-        throw new RentalNotFoundError(id);
-    }
+    const rental = await getRentalById(id);
 
     if (rental.returnDate == null) {
         throw new RentalNotReturnedError(id);
     }
 
     return rentalsRepository.deleteRental(id);
+}
+
+async function getRentalById(id) {
+    const result = await rentalsRepository.getRentalById(id);
+
+    if (result.rowCount === 0) {
+        throw new RentalNotFoundError(id);
+    }
+
+    return result.rows[0];
 }
 
 export const rentalsService = {
